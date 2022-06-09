b62bc589dbb9807845299b007abc07e0c9697a10
Filip Napiorkowski <roguepanthernoob@gmail.com>
2022-06-09 18:18:18 +0200

    Simple Calculator made in Java

---
+++ SimpleCalculator/src/one/filipify/SimpleCalculator.java
@@ -0,0 +1,14 @@
+package one.filipify;
+
+public class SimpleCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstValue = 26;
+		int secondValue = 34;
+		int sum = firstValue + secondValue;
+		
+		System.out.println("The result of adding first value and second value is " + sum);
+	}
+
+}
